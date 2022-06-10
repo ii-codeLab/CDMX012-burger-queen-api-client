30641e0747182be8ee4919edfbf0f850ff831c53
ii-codeLab <avalosvana@gmail.com>
2022-06-10 11:13:55 -0500

    menus component and its styles

---
+++ burguerqueen/src/components/Menus.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Menus() {
+  return (
+    <>
+      <div>
+          <section className='btns_container'>
+            <button id='btn_breakfast'>Breakfast</button>
+            <button id='btn_daytime'>Daytime</button>
+          </section>
+      </div>
+    </>
+  )
+}
+
+export default Menus
